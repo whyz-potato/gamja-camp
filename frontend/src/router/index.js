ae6d39271d4d6edeae245b079b8fb608cc5d9e4f
jaqkqh15 <jaqkqh15@naver.com>
2023-05-07 20:01:57 +0900

    .

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home'
 import Test from '@/views/Test'
-import Login from '@/views/user/Login'
-import Join from '@/views/user/Join'
+import Login from '@/views/member/Login'
+import Join from '@/views/member/Join'
+import Owner from '@/views/member/owner/Owner'
 
 Vue.use(VueRouter)
 
@@ -27,6 +28,11 @@ const routes = [
     path: '/join',
     name: 'Join',
     component: Join
+  },
+  {
+    path: '/owner',
+    name: 'Owner',
+    component: Owner
   },
 ]
 
