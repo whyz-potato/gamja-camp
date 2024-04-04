02d1aafe344edc2b10370e2dbe46cbf1ee814637
hs03130 <hs03130@ourlook.kr>
2024-04-05 00:52:02 +0900

    feat : CommentServiceTest 수정 및 추가

--- backend/src/test/java/whyzpotato/gamjacamp/service/CommentServiceTest.java
+++ backend/src/test/java/whyzpotato/gamjacamp/service/CommentServiceTest.java
@@ -1,91 +1,190 @@
 package whyzpotato.gamjacamp.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import whyzpotato.gamjacamp.controller.dto.CommentDto.CommentDetail;
 import whyzpotato.gamjacamp.controller.dto.CommentDto.CommentSaveRequest;
 import whyzpotato.gamjacamp.controller.dto.CommentDto.CommentUpdateRequest;
 import whyzpotato.gamjacamp.domain.member.Member;
-import whyzpotato.gamjacamp.domain.member.Role;
 import whyzpotato.gamjacamp.domain.post.Comment;
 import whyzpotato.gamjacamp.domain.post.Post;
-import whyzpotato.gamjacamp.domain.post.PostType;
 import whyzpotato.gamjacamp.repository.CommentRepository;
 import whyzpotato.gamjacamp.repository.MemberRepository;
 import whyzpotato.gamjacamp.repository.PostRepository;
 
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
+import java.time.LocalDateTime;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class CommentServiceTest {
 
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
-    @Autowired
+    @Mock
     private PostRepository postRepository;
-    @Autowired
+    @Mock
     private CommentRepository commentRepository;
-    @Autowired
+    @InjectMocks
     private CommentService commentService;
 
     @Test
     void 댓글_작성() {
-        Member member = memberRepository.save(Member.builder().account("hsy3130@test.com").username("hsy").picture(null).role(Role.CUSTOMER).build());
-        Post post = postRepository.save(Post.builder().writer(member).title("게시글제목").content("게시글내용").type(PostType.GENERAL).build());
-        CommentSaveRequest request = CommentSaveRequest.builder().content("첫번째댓글").upperComment(null).build();
+        Member member = spy(Member.builder().build());
+        given(member.getId()).willReturn(1L);
+        Post post = spy(Post.builder().comments(new ArrayList<>()).build());
+        given(post.getId()).willReturn(1L);
+        Comment comment = spy(Comment.builder().build());
+        given(comment.getId()).willReturn(1L);
+        given(memberRepository.findById(member.getId())).willReturn(Optional.ofNullable(member));
+        given(postRepository.findById(post.getId())).willReturn(Optional.ofNullable(post));
+        given(commentRepository.save(any(Comment.class))).willReturn(comment);
+        CommentSaveRequest request = CommentSaveRequest.builder().content("댓글").upperComment(null).build();
 
         Long commentId = commentService.save(member.getId(), post.getId(), request);
 
-        Comment comment = commentRepository.findById(commentId).get();
-        assertThat(comment.getContent()).isEqualTo("첫번째댓글");
-        assertThat(comment.getWriter().getId()).isEqualTo(member.getId());
-        assertThat(comment.getPost().getId()).isEqualTo(post.getId());
-        assertThat(comment.getUpperComment()).isNull();
-        assertThat(comment.getLowerComments()).isEmpty();
+        assertThat(commentId).isEqualTo(comment.getId());
+        assertThat(post.getComments().size()).isEqualTo(1);
     }
 
     @Test
     void 대댓글_작성() {
-        Member member = memberRepository.save(Member.builder().account("hsy3130@test.com").username("hsy").picture(null).role(Role.CUSTOMER).build());
-        Post post = postRepository.save(Post.builder().writer(member).title("게시글제목").content("게시글내용").type(PostType.GENERAL).build());
-        Comment upperComment = commentRepository.save(Comment.builder().writer(member).post(post).content("첫번째댓글").lowerComments(new ArrayList<Comment>()).build());
-        CommentSaveRequest request = CommentSaveRequest.builder().content("첫번째대댓글").upperComment(upperComment.getId()).build();
+        Member member = spy(Member.builder().build());
+        given(member.getId()).willReturn(1L);
+        Comment upperComment = spy(Comment.builder().lowerComments(new ArrayList<>()).build());
+        given(upperComment.getId()).willReturn(1L);
+        List<Comment> comments = new ArrayList<>();
+        comments.add(upperComment);
+        Post post = spy(Post.builder().comments(comments).build());
+        given(post.getId()).willReturn(1L);
+        Comment comment = spy(Comment.builder().build());
+        given(comment.getId()).willReturn(2L);
+        given(memberRepository.findById(member.getId())).willReturn(Optional.ofNullable(member));
+        given(postRepository.findById(post.getId())).willReturn(Optional.ofNullable(post));
+        given(commentRepository.findById(upperComment.getId())).willReturn((Optional.ofNullable(upperComment)));
+        given(commentRepository.save(any(Comment.class))).willReturn(comment);
+        CommentSaveRequest request = CommentSaveRequest.builder().content("대댓글").upperComment(upperComment.getId()).build();
 
         Long commentId = commentService.save(member.getId(), post.getId(), request);
 
-        Comment reComment = commentRepository.findById(commentId).get();
-        assertThat(reComment.getContent()).isEqualTo("첫번째대댓글");
-        assertThat(reComment.getWriter().getId()).isEqualTo(member.getId());
-        assertThat(reComment.getPost().getId()).isEqualTo(post.getId());
-        assertThat(reComment.getUpperComment().getId()).isEqualTo(upperComment.getId());
-        assertThat(commentRepository.findById(reComment.getUpperComment().getId()).get().getLowerComments()).hasSize(1);
-        assertThat(reComment.getLowerComments()).isEmpty();
+        assertThat(commentId).isEqualTo(comment.getId());
+        assertThat(post.getComments().size()).isEqualTo(2);
+        assertThat(upperComment.getLowerComments().size()).isEqualTo(1);
     }
 
     @Test
     void 댓글_수정() {
-        Member member = memberRepository.save(Member.builder().account("hsy3130@test.com").username("hsy").picture(null).role(Role.CUSTOMER).build());
-        Post post = postRepository.save(Post.builder().writer(member).title("게시글제목").content("게시글내용").type(PostType.GENERAL).build());
-        Comment comment = commentRepository.save(Comment.builder().writer(member).post(post).content("첫번째댓글").build());
+        Member member = spy(Member.builder().username("작성자").picture("https://picsum.photos/300/300").build());
+        given(member.getId()).willReturn(1L);
+        Post post = spy(Post.builder().build());
+        given(post.getId()).willReturn(5L);
+        LocalDateTime dateTime = LocalDateTime.now();
+        Comment comment = spy(Comment.builder().post(post).writer(member).content("댓글").lowerComments(new ArrayList<>()).build());
+        given(comment.getId()).willReturn(2L);
+        given(comment.getCreatedTime()).willReturn(dateTime);
         CommentUpdateRequest request = CommentUpdateRequest.builder().content("수정된내용").build();
+        given(memberRepository.findById(member.getId())).willReturn(Optional.ofNullable(member));
+        given(commentRepository.findById(comment.getId())).willReturn(Optional.ofNullable(comment));
+        given(commentRepository.save(comment.update(request))).willReturn(comment);
 
-        commentService.update(member.getId(), comment.getId(), request);
+        CommentDetail commentDto = commentService.update(member.getId(), comment.getId(), request);
 
-        Comment updateComment = commentRepository.findById(comment.getId()).get();
-        assertThat(updateComment.getContent()).isEqualTo("수정된내용");
+        assertThat(commentDto.getId()).isEqualTo(comment.getId());
+        assertThat(commentDto.getPost()).isEqualTo(5L);
+        assertThat(commentDto.getWriter().getId()).isEqualTo(member.getId());
+        assertThat(commentDto.getWriter().getUsername()).isEqualTo(member.getUsername());
+        assertThat(commentDto.getWriter().getProfile()).isEqualTo(member.getPicture());
+        assertThat(commentDto.getDate()).isEqualTo(dateTime.toLocalDate());
+        assertThat(commentDto.getTime()).isEqualTo(dateTime.toLocalTime());
+        assertThat(commentDto.getContent()).isEqualTo("수정된내용");
     }
 
     @Test
-    void 댓글_삭제() {
-        Member member = memberRepository.save(Member.builder().account("hsy3130@test.com").username("hsy").picture(null).role(Role.CUSTOMER).build());
-        Post post = postRepository.save(Post.builder().writer(member).title("게시글제목").content("게시글내용").type(PostType.GENERAL).build());
-        Comment comment = commentRepository.save(Comment.builder().writer(member).post(post).content("첫번째댓글").build());
+    void 댓글_삭제_실패() {
+        Member writer = spy(Member.builder().build());
+        given(writer.getId()).willReturn(1L);
+        Post post = spy(Post.builder().build());
+        given(post.getId()).willReturn(5L);
+        Comment comment = spy(Comment.builder().writer(writer).post(post).build());
+        given(comment.getId()).willReturn(1L);
+        Member member = spy(Member.builder().build());
+        given(member.getId()).willReturn(100L);
+        given(memberRepository.findById(member.getId())).willReturn(Optional.ofNullable(member));
+        given(commentRepository.findById(comment.getId())).willReturn(Optional.ofNullable(comment));
+        given(postRepository.findById(post.getId())).willReturn(Optional.ofNullable(post));
 
-        commentService.delete(member.getId(), comment.getId());
-
-        assertThat(commentRepository.findById(comment.getId()).isPresent()).isFalse();
+        assertThatThrownBy(()->commentService.delete(member.getId(), comment.getId())).isInstanceOf(NoSuchElementException.class);
     }
+
+
+    @Test
+    void 댓글_대댓글없음_삭제_성공() {
+        Member member = spy(Member.builder().build());
+        given(member.getId()).willReturn(1L);
+        Post post = spy(Post.builder().comments(new ArrayList<>()).build());
+        given(post.getId()).willReturn(5L);
+        Comment comment = spy(Comment.builder().writer(member).post(post).lowerComments(new ArrayList<>()).build());
+        given(comment.getId()).willReturn(1L);
+        post.getComments().add(comment);
+
+        given(memberRepository.findById(member.getId())).willReturn(Optional.ofNullable(member));
+        given(commentRepository.findById(comment.getId())).willReturn(Optional.ofNullable(comment));
+        given(postRepository.findById(post.getId())).willReturn(Optional.ofNullable(post));
+        doNothing().when(commentRepository).delete(eq(comment));
+
+        assertThatCode(()->commentService.delete(member.getId(), comment.getId())).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 댓글_대댓글있음_삭제_성공() {
+        Member member = spy(Member.builder().build());
+        given(member.getId()).willReturn(1L);
+        Post post = spy(Post.builder().comments(new ArrayList<>()).build());
+        given(post.getId()).willReturn(5L);
+        Comment comment = spy(Comment.builder().writer(member).post(post).lowerComments(new ArrayList<>()).build());
+        given(comment.getId()).willReturn(1L);
+        Comment lowerComment1 = Comment.builder().upperComment(comment).build();
+        Comment lowerComment2 = Comment.builder().upperComment(comment).build();
+        post.getComments().add(comment);
+        post.getComments().add(lowerComment1);
+        post.getComments().add(lowerComment2);
+        comment.getLowerComments().add(lowerComment1);
+        comment.getLowerComments().add(lowerComment2);
+        given(memberRepository.findById(member.getId())).willReturn(Optional.ofNullable(member));
+        given(commentRepository.findById(comment.getId())).willReturn(Optional.ofNullable(comment));
+        given(postRepository.findById(post.getId())).willReturn(Optional.ofNullable(post));
+        doNothing().when(commentRepository).deleteAll(any());
+        doNothing().when(commentRepository).delete(eq(comment));
+
+        assertThatCode(()->commentService.delete(member.getId(), comment.getId())).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 대댓글_삭제_성공() {
+        Member member = spy(Member.builder().build());
+        given(member.getId()).willReturn(1L);
+        Post post = spy(Post.builder().comments(new ArrayList<>()).build());
+        given(post.getId()).willReturn(5L);
+        Comment upperComment = Comment.builder().lowerComments(new ArrayList<>()).build();
+        Comment comment = spy(Comment.builder().writer(member).post(post).upperComment(upperComment).lowerComments(new ArrayList<>()).build());
+        given(comment.getId()).willReturn(2L);
+        post.getComments().add(upperComment);
+        post.getComments().add(comment);
+        upperComment.getLowerComments().add(comment);
+        given(memberRepository.findById(member.getId())).willReturn(Optional.ofNullable(member));
+        given(commentRepository.findById(comment.getId())).willReturn(Optional.ofNullable(comment));
+        given(postRepository.findById(post.getId())).willReturn(Optional.ofNullable(post));
+        doNothing().when(commentRepository).delete(eq(comment));
+
+        assertThatCode(() -> commentService.delete(member.getId(), comment.getId())).doesNotThrowAnyException();
+    }
+
 }
